bed734bc23a03fce6cdca3e0c3bf4431bb29e185
Tinghan Chen(Allen) <chentinghan@outlook.com>
2021-10-14 10:22:19 +1100

    chore: any -> unkown

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value) => (value === 0 ? false : !value);
-export const cleanObject = (object) => {
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(object).forEach((key) => {
+    //@ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      //@ts-ignore
       delete result[key];
     }
   });
@@ -13,18 +15,18 @@ export const cleanObject = (object) => {
 };
 
 //custom hook最大的特征就是里面会有hook用到
-export const useMount = (callback) => {
+export const useMount = (callback: () => void) => {
   useEffect(() => {
     callback();
   }, []);
 };
 
-export const useDebounce = (value, delay) => {
+export const useDebounce = (value: unknown, delay?: number): any => {
   const [debounceValue, setDebounceValue] = useState(value);
   useEffect(() => {
     //每次在value变化以后设置一个定时器
     const timeout = setTimeout(() => setDebounceValue(value), delay);
-    //每次再上一个useffect运行完以后在运行
+    //每次再上一个useEffect运行完以后在运行
     return () => {
       clearTimeout(timeout);
     };
