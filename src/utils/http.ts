305343cd1fb1affb834383a3b9bba1bf0e4b23c5
Tinghan Chen(Allen) <chentinghan@outlook.com>
2021-10-19 13:57:07 +1100

    style: optimize project list page

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,6 +1,7 @@
 import qs from "qs";
 import * as auth from "auth-provider";
-import { useAuth } from "../context/auth-context";
+import { useAuth } from "context/auth-context";
+import { type } from "os";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -16,16 +17,19 @@ export const http = async (
   const config = {
     method: "GET",
     headers: {
-      Authorization: token ? `Bearer${token}` : "",
+      Authorization: token ? `Bearer ${token}` : "",
       "Content-Type": data ? "application/json" : "",
     },
     ...customConfig,
   };
+
   if (config.method.toUpperCase() === "GET") {
     endpoint += `?${qs.stringify(data)}`;
   } else {
     config.body = JSON.stringify(data || {});
   }
+
+  // axios 和 fetch 的表现不一样，axios可以直接在返回状态不为2xx的时候抛出异常
   return window
     .fetch(`${apiUrl}/${endpoint}`, config)
     .then(async (response) => {
@@ -34,7 +38,7 @@ export const http = async (
         window.location.reload();
         return Promise.reject({ message: "请重新登录" });
       }
-      const data = response.json();
+      const data = await response.json();
       if (response.ok) {
         return data;
       } else {
@@ -43,9 +47,14 @@ export const http = async (
     });
 };
 
-//自动存入token当请求api时
+// JS 中的typeof，是在runtime时运行的
+// return typeof 1 === 'number'
+
+// TS 中的typeof，是在静态环境运行的
+// return (...[endpoint, config]: Parameters<typeof http>) =>
 export const useHttp = () => {
   const { user } = useAuth();
+  // utility type 的用法：用泛型给它传入一个其他类型，然后utility type对这个类型进行某种操作
   return (...[endpoint, config]: Parameters<typeof http>) =>
     http(endpoint, { ...config, token: user?.token });
 };
